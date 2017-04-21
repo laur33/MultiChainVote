a3db50443e95d6cb877fdf3e640da3cec62755a4
Laurence Dean <14758610@student.curtin.edu.au>
2017-04-21 19:57:29 +0800

    Can connect to block chain on click

--- MultichainVoting/MainWindow.cs
+++ MultichainVoting/MainWindow.cs
@@ -11,14 +11,7 @@ public partial class MainWindow : Gtk.Window
 {
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
 	{
-
 		Build();
-
-		var client = new MultiChainClient("192.168.218.128", 6820, false, "multichainrpc", "A3u8rr7R7i8wCtcYEfMH41aCjWcapXGdxhmdHhVtMGvs", "test");
-
-	
-
-
 	}
 
 	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
@@ -27,4 +20,24 @@ public partial class MainWindow : Gtk.Window
 		a.RetVal = true;
 	}
 
+	protected void OnClientConnectClicked(object sender, EventArgs e)
+	{
+			// Call task to connect to client blockchain
+			var task = Task.Run(async () =>
+			{
+								await connectToChain();
+			});
+			task.Wait();
+	}
+
+	internal async Task connectToChain()
+	{
+		var client = new MultiChainClient("192.168.218.128", 6820, false, "multichainrpc", "A3u8rr7R7i8wCtcYEfMH41aCjWcapXGdxhmdHhVtMGvs", "test");
+
+		// get server info...
+		Console.WriteLine("*** getinfo ***");
+		var info = await client.GetInfoAsync();
+		info.AssertOk();
+		Console.WriteLine("Chain: {0}, difficulty: {1}", info.Result.ChainName, info.Result.Difficulty);
+		Console.WriteLine();	}
 }
--- MultichainVoting/gtk-gui/MainWindow.cs
+++ MultichainVoting/gtk-gui/MainWindow.cs
@@ -3,13 +3,50 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.UIManager UIManager;
+
+	private global::Gtk.VBox vbox1;
+
+	private global::Gtk.MenuBar menubar1;
+
+	private global::Gtk.Button clientConnect;
+
 	protected virtual void Build()
 	{
 		global::Stetic.Gui.Initialize(this);
 		// Widget MainWindow
+		this.UIManager = new global::Gtk.UIManager();
+		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup("Default");
+		this.UIManager.InsertActionGroup(w1, 0);
+		this.AddAccelGroup(this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.vbox1 = new global::Gtk.VBox();
+		this.vbox1.Name = "vbox1";
+		this.vbox1.Spacing = 6;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.UIManager.AddUiFromString("<ui><menubar name=\'menubar1\'/></ui>");
+		this.menubar1 = ((global::Gtk.MenuBar)(this.UIManager.GetWidget("/menubar1")));
+		this.menubar1.Name = "menubar1";
+		this.vbox1.Add(this.menubar1);
+		global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.menubar1]));
+		w2.Position = 0;
+		w2.Expand = false;
+		w2.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.clientConnect = new global::Gtk.Button();
+		this.clientConnect.CanFocus = true;
+		this.clientConnect.Name = "clientConnect";
+		this.clientConnect.UseUnderline = true;
+		this.clientConnect.Label = global::Mono.Unix.Catalog.GetString("clientConnect");
+		this.vbox1.Add(this.clientConnect);
+		global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.clientConnect]));
+		w3.Position = 1;
+		w3.Expand = false;
+		w3.Fill = false;
+		this.Add(this.vbox1);
 		if ((this.Child != null))
 		{
 			this.Child.ShowAll();
@@ -18,5 +55,6 @@ public partial class MainWindow
 		this.DefaultHeight = 300;
 		this.Show();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler(this.OnDeleteEvent);
+		this.clientConnect.Clicked += new global::System.EventHandler(this.OnClientConnectClicked);
 	}
 }
